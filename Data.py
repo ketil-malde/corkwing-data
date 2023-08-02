5f99cb62bcff722ae9fcb3a8db4a40a027a05eab
Ketil Malde <ketil.malde@imr.no>
2023-08-02 10:57:28 +0000

    Switch to 'shallow' dataset

--- Data.py
+++ Data.py
@@ -6,12 +6,13 @@ import os.path
 import xml.etree.ElementTree as et
 import cv2
 import numpy as np
+import shutil
 
 defaultconf = {
-    'annotations': ['All_annotations']  # ['Nest1_finished', '16_Nest2']
+    'annotations': ['annotations']  # ['Nest1_finished', '16_Nest2']
     }
 
-datasrc = 'dedun.hi.no:/data/deep/CoastVision/Updated_dataset_object_detection_and_segmentation'
+datasrc = 'dedun.hi.no:/data/deep/CoastVision/Shallow-20230727'
 
 def collectpts(pts):
     '''Convert to list of pairs of floats'''
@@ -19,23 +20,26 @@ def collectpts(pts):
 
 def extract_xml(anns):
     '''Parse xml annotation, output csv table and masks'''
-    res = {}
+    res = []
     assert(isinstance(anns,list))
 
     for fn in anns:
         print(f'Parsing  {fn}.xml')
         tree = et.parse(f'{fn}.xml')
         r = tree.getroot()
+
+        # Build a dict of all "tasks" listed, link task_id to task name
         tasks = {}
         for ts in r.findall("meta/project/tasks/task"):
-            mydir = ts.find("name").text
-            tasks[ts.find("id").text] = mydir
-            res[mydir] = []
+            taskname = ts.find("name").text
+            tasks[ts.find("id").text] = taskname
 
         for ch in r.findall("image"):
             cur = []
             imfile = ch.attrib['name']
-            mydir = tasks[ch.attrib['task_id']]
+            mytask = tasks[ch.attrib['task_id']]
+            mysubset = ch.attrib['subset']
+ 
             for e in ch:
                 if e.tag == 'polygon':
                     # image/polygon/attribute(sex,body_shape_guessed,main_orientation,completely_in_frame,type)
@@ -54,8 +58,8 @@ def extract_xml(anns):
                     if attr.attrib['name'] == 'sex' and not attr.text == 'unknown':
                         sex = "_"+attr.text
                 if species == "cuckoo" or species == "corkwing" and sex is not None: species += sex
-                cur.append((imfile, species , pts))
-            res[mydir] += cur
+                cur.append((imfile, mytask, mysubset, species , pts))
+            res += cur
     # print(res)
     return res
 
@@ -91,20 +95,23 @@ class Data:
         annos = extract_xml([prefixdir+'/'+s for s in self.config['annotations']])
         os.mkdir("images")
         with open('annotations.csv', 'w') as f:
-            for mydir in annos:
-                print(mydir, len(annos[mydir]))
-                # todo: copy images, create masks, output annotations to f
-                # for im,spec,pts in anns:
-                for im, spec, pts in annos[mydir]:
+            # for mydir in annos:
+                # todo: create masks
+                for im, task, sub, spec, pts in annos:
                     imname = rename(im)
                     xs = [ float(x) for (x,y) in pts ]
                     ys = [ float(y) for (x,y) in pts ]
                     bbox = ( min(xs), min(ys), max(xs), max(ys) )
-                    if os.path.exists(f'{prefixdir}/{mydir}/{im}'):
-                        os.system(f'cp "{prefixdir}/{mydir}/{im}" images/{imname}')
+                    if os.path.exists(f'{prefixdir}/{task}/{im}'):
+                        shutil.copyfile(f"{prefixdir}/{task}/{im}", f"images/{imname}")
+                        f.write(f'{imname}\t{spec}\t{bbox}\n')
+                    elif os.path.exists(f'{prefixdir}/images/{sub}/{im}'):
+                        # new style CVAT stupid format fuckup
+                        shutil.copyfile(f"{prefixdir}/images/{sub}/{im}", f"images/{imname}")
                         f.write(f'{imname}\t{spec}\t{bbox}\n')
                     else:
-                        print(f"Can't find: {prefixdir}/{mydir}/{im}!")
+                        print(f"Can't find {im} in {prefixdir}/{task} or {prefixdir}/images/{sub}!")
+                        exit(-1)
 
     def validate(self):
         pass
